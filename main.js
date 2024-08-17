9f4820d96f6cc65b354b89680bbb5aa247952ed3
ismoilnuriddinov <umidafayziyeva89@gmail.com>
2024-08-17 13:43:05 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+let ism = prompt("string typidagi malumotni kiriting ");
+let familiya = prompt("number typidagi malumotni kiriting ");
+let yoshingiz = prompt("boolean typidagi malumotni kiriting ");
+
+
+console.log("ism:" + "ism");
+console.log("familiya:" + "familiya");
+console.log("yoshingiz:" + "yoshingiz");
+
+
+let matn = "avazbek";
+let aniqlanmagan = undefined;
+
+let tasdiqlash = confirm("kiritgan malumotingiz togrimi");
+
+
+
+console.log(tasdiqlash);
+
